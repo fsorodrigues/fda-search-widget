82a1372b1719aca8488bfe3ad4eceef845ded8be
fsorodrigues <fsorodrigues@gmail.com>
2023-10-18 21:42:17 +1300

    Modify type definition

--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -1,6 +1,10 @@
-export interface Data {
+import type { DSVParsedArray } from "d3-dsv";
+
+export interface DataRow {
   drug_slug: string;
   slug: string;
   flag_1: boolean;
   flag_2: boolean;
 }
+
+interface Data extends DSVParsedArray<T> {}
