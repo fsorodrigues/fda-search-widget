243fee1c2a0da6af2cd76c44a19e6e53577133aa
fsorodrigues <fsorodrigues@gmail.com>
2023-10-19 22:12:31 +1300

    Data prop, add input element

--- src/lib/SearchBar.svelte
+++ src/lib/SearchBar.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import type { Data } from "../types/data";
+
+  export let data: Data[];
   export let placeholder: string = "Search...";
 </script>
 
-<div>
-  {placeholder}
+<div class="search-bar">
+  <input type="search" {placeholder} aria-label="Search" on:change={() => {}} />
 </div>
