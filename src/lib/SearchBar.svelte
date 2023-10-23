8d4f4e1e78d37ce2b7dbcbc10e73a3bebbe5d0e4
fsorodrigues <fsorodrigues@gmail.com>
2023-10-23 19:18:19 +1300

    Starting work on event listeners

--- src/lib/SearchBar.svelte
+++ src/lib/SearchBar.svelte
@@ -8,7 +8,8 @@
   export let data: Data;
   export let placeholder: string = "Search...";
 
-  const search = searcher(data);
+  let query: string;
+  const search: any = searcher(data);
 </script>
 
 <div class="search-bar">
@@ -16,10 +17,35 @@
     type="search"
     {placeholder}
     aria-label="Search"
-    on:change={() => {
-      // console.log();
-      search.find("aspirin");
+    on:input={() => {
+      // console.log(search.find("aspirin"));
+      console.log("here");
     }}
+    on:change={() => {
+      // console.log(search.find("aspirin"));
+      console.log("here");
+    }}
+    bind:value={query}
   />
-  {data.length}
 </div>
+
+<style>
+  .search-bar {
+    width: 100%;
+  }
+
+  .search-bar input {
+    width: 100%;
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 0.5rem;
+    font-size: 1rem;
+    font-family: inherit;
+    transition: all 0.2s ease-in-out;
+  }
+
+  .search-bar input:focus {
+    outline: none;
+    border-color: #000;
+  }
+</style>
