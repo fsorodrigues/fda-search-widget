fd94e24f6706e8bfbae074f414087132b31f4c01
fsorodrigues <fsorodrigues@gmail.com>
2023-10-24 22:29:11 +1300

    New data format
    Matching flags available in database

--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -3,8 +3,14 @@ import type { DSVParsedArray } from "d3-dsv";
 interface DataRow {
   drug_slug: string;
   drug: string;
-  flag_1: boolean;
-  flag_2: boolean;
+  treatment_condition: string;
+  url: string;
+  black_box: string;
+  flag_evidence: boolean;
+  flag_blinding: boolean;
+  flag_replication: boolean;
+  flag_control: boolean;
+  flag_surrogate: boolean;
 }
 
 interface Data extends DSVParsedArray<T> {}
