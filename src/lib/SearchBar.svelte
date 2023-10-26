35832041e74374ed48eabd17593f531069bd1af2
fsorodrigues <fsorodrigues@gmail.com>
2023-10-26 23:20:47 +1300

    Add search store to SearchBar

--- src/lib/SearchBar.svelte
+++ src/lib/SearchBar.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+  // import from node_modules
+  import { onDestroy } from "svelte";
+
+  // import local modules
+  import { createSearchStore } from "../stores/search";
+
   // import searcher
-  import searcher from "../utils/searcher";
+  import { finder } from "../utils/searcher";
 
   // import interface
   import type { Data } from "../types/data";
@@ -8,18 +14,30 @@
   export let data: Data;
   export let placeholder: string = "Search...";
 
-  const search = searcher(data);
+  const searchStore: any = createSearchStore(data);
+  const unsubscribe = searchStore.subscribe((d: any) =>
+    finder(d)
+  );
+
+  onDestroy(() => {
+    unsubscribe();
+  });
+
+  $: console.log($searchStore);
+
+  // const search = searcher(data);
 </script>
 
 <div class="search-bar">
   <input
+    aria-label="Search"
     type="search"
     {placeholder}
-    aria-label="Search"
-    on:change={() => {
-      // console.log();
-      search.find("aspirin");
-    }}
+    bind:value={$searchStore.search}
   />
+  <!-- on:change={() => { -->
+  <!--   console.log("here"); -->
+  <!--   // search.find("aspirin"); -->
+  <!-- }} -->
   {data.length}
 </div>
