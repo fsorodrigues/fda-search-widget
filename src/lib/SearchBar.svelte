813c14704f469a3fa80d326a4bdc6583f0c77a22
fsorodrigues <fsorodrigues@gmail.com>
2023-10-30 19:45:34 +1300

    Adding a wrapper to options
    div.suggestions used for better positioning of search suggestion options

--- src/lib/SearchBar.svelte
+++ src/lib/SearchBar.svelte
@@ -35,26 +35,45 @@
     {placeholder}
     bind:value={$searchStore.search}
   />
-  {#if !$searchStore.selected}
-    {#each $searchStore.filtered as option, i}
-      <div
-        class="option"
-        class:highlight={i === 0}
-        on:click={() => {
-          $searchStore.selected = option.obj;
-          $searchStore.search = option.target;
-        }}
-        on:keydown={() => console.log(option.target)}
-        tabindex="0"
-        role="button"
-      >
-        <span>{i} {option.obj.drug}</span>
-      </div>
-    {/each}
+    <div class="suggestions">
+      {#each $searchStore.filtered as option, i}
+        <div
+          class="option"
+          class:highlight={i === 0}
+          on:click={() => {
+            $searchStore.selected = option.obj;
+            $searchStore.search = option.target;
+          }}
+          on:keydown={() => console.log(option.target)}
+          tabindex="0"
+          role="button"
+        >
+          <span>{i} {option.obj.drug}</span>
+        </div>
+      {/each}
+    </div>
   {/if}
 </div>
 
 <style>
+  .search-bar {
+    --search-bar-height: 35px;
+    position: relative;
+  }
+
+  .search-bar input {
+    height: var(--search-bar-height);
+  }
+
+  .suggestions {
+    position: absolute;
+    top: var(--search-bar-height);
+  }
+
+  .option {
+    background-color: #fff;
+  }
+
   .highlight {
     background-color: #f5f5f5;
   }
