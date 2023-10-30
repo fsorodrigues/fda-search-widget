d90b6a74cecede330f89015ed3f69e92ea3a0e4a
fsorodrigues <fsorodrigues@gmail.com>
2023-10-30 23:08:36 +1300

    Add labels and grid layout

--- src/lib/Reasons.svelte
+++ src/lib/Reasons.svelte
@@ -5,12 +5,23 @@
   // properties
   export let properties: (keyof DataRow)[];
   export let value: DataRow;
+
+  function parseEvidenceFlag(value: boolean) {
+    return value ? "rigorous" : "not rigorous";
+  }
 </script>
 
 <div class="row">
-  {#each properties as flag, i}
-    <div class="flag flag-{i}">
-      <span>{value[flag]}</span>
-    </div>
-  {/each}
+  <div class="opening">
+    Reasons testing was <span
+      >{parseEvidenceFlag(value.flag_evidence)}</span
+    >
+  </div>
+  <div class="reasons">
+    {#each properties.filter((d) => d !== "flag_evidence") as flag, i}
+      <div class="flag flag-{i}">
+        <span>{value[flag]}</span>
+      </div>
+    {/each}
+  </div>
 </div>
--- src/lib/Testing.svelte
+++ src/lib/Testing.svelte
@@ -5,4 +5,22 @@
   export let value: boolean;
 </script>
 
-<div class="row">{value}</div>
+<div class="row">
+  <div class="label">Condition treated</div>
+  <div class="value">{value}</div>
+</div>
+
+<style>
+  .row {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+
+  .row .label {
+    grid-column: 1 / span 1;
+  }
+
+  .row .value {
+    grid-column: 2 / span 1;
+  }
+</style>
