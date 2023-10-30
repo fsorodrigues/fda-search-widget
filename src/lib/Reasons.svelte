b9e25faf59ee20fcc0df16bd7488d2b884688fe6
fsorodrigues <fsorodrigues@gmail.com>
2023-10-30 19:41:13 +1300

    Render content in rows
    Values are dynamically rendered in rows

--- src/lib/Reasons.svelte
+++ src/lib/Reasons.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
+  import type { DataRow } from "../types/data";
   import "./global.css";
+
+  // properties
+  export let value: DataRow;
 </script>
 
-<div class="row"></div>
+<div class="row">
+  {#each properties as flag, i}
+    <div class="flag flag-{i}">
+      <span>{value[flag]}</span>
+    </div>
+  {/each}
+</div>
