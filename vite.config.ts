98dd967430f99a674411607341bc8497e41c2640
fsorodrigues <fsorodrigues@gmail.com>
2023-11-13 23:58:26 +1300

    Set root path
    Altering vite config to deploy properly to GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-})
+export default defineConfig(({ command }) => {
+  if (command === "serve") return { plugins: [svelte()] };
+  else
+    return { base: "/fda-search-widget/", plugins: [svelte()] };
+});
