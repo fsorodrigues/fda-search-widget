b70b791e4b7074e9dd93780efbd09ba18adc11c4
fsorodrigues <fsorodrigues@gmail.com>
2023-11-19 18:43:38 +1300

    Map also returns color
    Use mapper to return box color according to value

--- src/types/mappers.d.ts
+++ src/types/mappers.d.ts
@@ -1,2 +1,6 @@
-export type FlagMap = Map<boolean | string, string>;
+export interface MapValue {
+  value: string;
+  color: string;
+}
+export type FlagMap = Map<boolean | string, string | MapValue>;
 export type MapMap = Map<string, FlagMap>;
