28f6a59b5a172c0f645664f1d2df2e00f2922d1f
fsorodrigues <fsorodrigues@gmail.com>
2023-11-22 22:55:23 +1300

    Prettier indenting

--- src/App.svelte
+++ src/App.svelte
@@ -63,9 +63,10 @@
       in:slide={{ duration: 250, easing: quadOut }}
       out:slide={{ duration: 100, easing: quadOut }}
     >
-    <label for="search-input" class="label"
-      >Was my drug rigorously tested?</label>
-    <p class="subhead">Look up drugs approved from 2013-22</p>
+      <label for="search-input" class="label"
+        >Was my drug rigorously tested?</label
+      >
+      <p class="subhead">Look up drugs approved from 2013-22</p>
     </div>
   {/if}
   {#if data}
@@ -87,11 +88,14 @@
         in:drawer={{ duration: 500, easing: quadOut }}
         out:slide={{ duration: 100, easing: quadOut }}
       >
-      <Condition value={selected.treatment_condition} />
-      <Warning value={selected.black_box} />
-      <Testing value={selected.meets_standard} />
-      <Reasons value={selected} properties={reasonProperties} />
-      <Reset {searchStore} {suggestedIndex}></Reset>
+        <Condition value={selected.treatment_condition} />
+        <Warning value={selected.black_box} />
+        <Testing value={selected.meets_standard} />
+        <Reasons
+          value={selected}
+          properties={reasonProperties}
+        />
+        <Reset {searchStore} {suggestedIndex}></Reset>
       </div>
     {/if}
   </div>
